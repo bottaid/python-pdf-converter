3b1d4fa88444a22ecf99049abecda452d143b156
bottaid <dariobottai@gmail.com>
2017-09-19 21:45:53 +0200

    Add utils/__init__.py

---
+++ src/conversione.py
@@ -0,0 +1,21 @@
+from model import esercizio
+
+def main():
+    '''
+    controllaArgomenti()
+    controllaEstensioneFile()
+    controllaDimensioneTabella()
+    estrazioneDocumento()
+    estrazioneTabella()
+    controllaCsvEsistente()
+    creaNuovoCsv()
+    '''
+    oggettoEsercizio = Esercizio()
+    
+    Esercizio().A()
+
+    print ("Hello world")
+    
+
+if __name__ == '__main__':
+    main()
---
+++ src/model/esercizio.py
@@ -0,0 +1,11 @@
+class Esercizio(object):
+    def __init__(self):
+        self.a = ''
+
+    def A(self):
+        self.a = 'A'
+        print (self.a)
+
+    
+    def presentazione():
+        print ("sono un esercizio!")
